463c9c613e185e2bd56c3f357a263af97551549b
Matthew Mahan <themaskedkid321@gmail.com>
2023-10-10 17:05:39 -0500

    Matrix operations add, dot product, multiply

--- src/main/java/com/multilayer/Main.java
+++ src/main/java/com/multilayer/Main.java
@@ -4,27 +4,45 @@ class Main {
 
     public static void main(String[] args) {
 
+        // basic test matrices
+        Matrix testM1 = new Matrix(3, 3);
+        Matrix testM2 = new Matrix(new float[][] {{1f, 2f, 3f},
+                                                  {4f, 5f, 6f},
+                                                  {7f, 8f, 9f}});
+        Matrix testM3 = new Matrix(new float[][] {{1f, 2f},
+                                                  {3f, 4f}});
+
         // Standardized matrices from assignment pdf: 
         Matrix L1TestWeights = new Matrix(new float[][] {{-0.21f, 0.72f, -0.25f, 1f}, 
                                                          {-0.94f, -0.41f, -0.47f, 0.63f},
                                                          {0.15f, 0.55f, -0.49f, -0.75f}});
-
         Matrix L1TestBiases = new Matrix(new float[][] {{0.1f},
                                                         {-0.36f},
                                                         {-0.31f}});
-
         Matrix L2TestWeights = new Matrix(new float[][] {{0.76f, 0.48f, -0.73f},
                                                          {0.34f, 0.89f, -0.23f}});
-
         Matrix L2TestBiases = new Matrix(new float[][] {{0.16f},
                                                         {-0.46f}});
 
         float testLearningRate = 10f;
 
+        System.out.println("Assignment matrices ----------");
         System.out.println(L1TestWeights.toString() + "\n");
         System.out.println(L1TestBiases.toString() + "\n");
         System.out.println(L2TestWeights.toString() + "\n");
         System.out.println(L2TestBiases.toString() + "\n");
+
+        System.out.println("Basic matrices ----------");
+        System.out.println(testM1.add(testM2) + "\n");
+        System.out.println(testM2.getRow(1).toString() + "\n");
+        System.out.println(testM2.getColumn(1).toString() + "\n");
+        try {
+            System.out.println(testM2.getRow(1).dotProduct(testM2.getColumn(1)) + "\n");
+        } catch (MatrixException e) {
+            System.out.println(e.toString());
+        }
+        System.out.println(testM2.matrixMultiply(testM2) + "\n");
+        System.out.println(testM3.matrixMultiply(testM3) + "\n");
 
     }
 
--- src/main/java/com/multilayer/Matrix.java
+++ src/main/java/com/multilayer/Matrix.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 class Matrix {
 
+    private static short DEBUG = 0;
+
     // the first bracket shall refer to a row 
     // the second bracket shall refer to a column
-    // m is the height, n is the width for an n x m matrix
+    // m is the height, n is the width for an m x n matrix
     // an item in the matrix is referred to by a_i,j where 
     // i traverses the matrix vertically downward and 
     // j traverses the matrix horizontally rightward 
@@ -24,9 +26,21 @@ class Matrix {
     }
 
     // init a matrix m x n with all zeros
-    public Matrix(int width, int height) {
+    // I don't want users to have access to a matrix with an array of null values
+    public Matrix(int height, int width) {
         float[][] values = fillAllZeros(new float[height][width]);
         this.setValues(values);
+    }
+
+    // this is just to initialize a "blank" array for a new matrix
+    // not to be used elsewhere
+    private static float[][] fillAllZeros(float[][] valuesArray) {
+        for (int i = 0; i < valuesArray.length; i++) {
+            for (int j = 0; j < valuesArray[0].length; j++) {
+                valuesArray[i][j] = 0;
+            }
+        }
+        return valuesArray;
     }
 
     // accessors and mutators 
@@ -36,6 +50,18 @@ class Matrix {
 
     public float getValueAt(int i, int j) {
         return this.values[i][j];
+    }
+
+    public Matrix getRow(int i) {
+        return new Matrix(new float[][] {this.values[i]});
+    }
+
+    public Matrix getColumn(int j) {
+        Matrix output = new Matrix(this.getHeight(), 1);
+        for (int i = 0; i < this.getHeight(); i++) {
+            output.setValueAt(this.getValueAt(i, j), i, 0);
+        }
+        return output;
     }
     
     public void setValues(float[][] values) {
@@ -70,6 +96,27 @@ class Matrix {
         return this.values.length;
     }
 
+    public boolean isSameDimensionsAs(Matrix other) {
+        if ((this.getHeight() == other.getHeight()) && (this.getWidth() == other.getWidth())) {
+            return true; 
+        }
+        return false;
+    }
+
+    public boolean isRowVector() {
+        if (this.getHeight() == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isColumnVector() {
+        if (this.getWidth() == 1) {
+            return true;
+        }
+        return false;
+    }
+
     public String toString() {
         String output = "";
         for (int i = 0; i < this.getHeight(); i++) {
@@ -81,15 +128,98 @@ class Matrix {
         return output;
     }
 
-    // this is just to initialize a "blank" array for a new matrix
-    // not to be used elsewhere
-    private static float[][] fillAllZeros(float[][] valuesArray) {
-        for (int i = 0; i < valuesArray.length; i++) {
-            for (int j = 0; j < valuesArray[0].length; j++) {
-                valuesArray[i][j] = 0;
+    public Matrix add(Matrix other) {
+
+        try {
+
+            if (this.isSameDimensionsAs(other)) {
+
+                Matrix sum = new Matrix(this.getHeight(), this.getWidth());
+
+                for (int i = 0; i < this.getHeight(); i++) {
+                    for (int j = 0; j < this.getWidth(); j++) {
+                        sum.setValueAt(this.getValueAt(i, j) + other.getValueAt(i, j), i, j);
+                    }
+                }
+
+                return sum;
+            } else {
+                throw new MatrixException("Added matrices are not the same dimensions.");
             }
+            
+        } catch (MatrixException e) {
+            System.out.println(e.toString());
         }
-        return valuesArray;
+
+        return null;
+
+    }
+
+    public float dotProduct(Matrix other) throws MatrixException {
+
+        if (DEBUG >= 3) {
+            System.out.println("Dotting left matrix: \n" + this.toString());
+            System.out.println("Against right matrix: \n" + other.toString());
+        }
+
+        // dot products technically can only be applied to a row "this" vector and a column "other" vector
+        // the width of "this" must be the same as the height of "other"
+
+        if (this.isRowVector() && other.isColumnVector()) {
+
+            if (this.getWidth() == other.getHeight()) {
+
+                float product = 0f;
+
+                for (int i = 0; i < other.getHeight(); i++) {
+                    product += this.getValueAt(0, i) * other.getValueAt(i, 0);
+                }
+
+                if (DEBUG >= 2) {
+                    System.out.println("Dotted product is: " + product + "\n");
+                }
+
+                return product;
+
+            } else {
+                throw new MatrixException("Dot product can only be performed on a row vector and column vector. The row vector width must be equal to the column vector height.");
+            }
+
+        } else {
+            throw new MatrixException("Dot product can only be performed on a row vector against a column vector. Did you mean to multiply them the other way around?");
+        }
+
+    }
+
+    public Matrix matrixMultiply(Matrix other) {
+
+        // the width of the left matrix must be the same as the height of the right matrix
+        // the resulting matrix will have the width of the right matrix and the height of the left matrix
+        
+        try {
+
+            if (this.getWidth() == other.getHeight()) {
+
+                Matrix product = new Matrix(this.getHeight(), other.getWidth());
+
+                for (int i = 0; i < product.getHeight(); i++) {
+                    for (int j = 0; j < product.getWidth(); j++) {
+                        product.setValueAt(this.getRow(i).dotProduct(other.getColumn(j)), i, j);
+                    }
+                }
+
+                return product;
+
+            } else {
+                throw new MatrixException("Matrix multiplication can only be performed if the left matrix width is the same as the right matrix height.");
+            }
+
+        } catch (MatrixException e) {
+            System.out.println(e.toString());
+        }
+
+        return null;
+
     }
 
 }
